f0476bdff30de7f06032f4125b8b28604937db6f
Frederic Viry <thedede00@gmail.com>
2016-09-14 18:59:03 +0200

    Compilation routine improved.

--- compile.sh
+++ compile.sh
@@ -4,15 +4,20 @@ rm -r compiled/classes/*
 rm -r release/doc/*
 rm -r release/*.jar
 echo 'Done !'
+# Extract Apache Commons Math
+echo 'Extract Apache Commons Math...'
+jar xf src/lib/commons-math3-3.6.1.jar org
+mv org compiled/classes/
+echo 'Done !'
 # Library compilation
 echo 'Compile the library...'
 javac -cp release/lib/commons-math3-3.6.1.jar:compiled/classes/ -d compiled/classes src/fr/imag/ppplib/*.java -Xlint:unchecked
 echo 'Done !'
 # JAR generation
 echo 'JAR generation...'
-jar cfm release/3plib.jar src/manifest/MANIFEST.MF -C compiled/classes/ .
+jar cf release/3plib.jar -C compiled/classes/ .
 echo 'Done !'
 # Documentation generation
 echo 'Documentation generation...'
-javadoc -quiet -d release/doc src/fr/imag/ppplib/*.java -classpath release/lib/commons-math3-3.6.1.jar
+javadoc -quiet -d release/doc src/fr/imag/ppplib/*.java -classpath src/lib/commons-math3-3.6.1.jar
 echo 'Done !'
